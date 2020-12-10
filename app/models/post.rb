4ee6bc2489a0a87ee7781388ba6bb47034fe8ed7
cinna-codes <somethingfellipickeditup@gmail.com>
2020-12-10 14:43:32 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,25 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
+
+  ### `before_create` is very close to `before_save` with one major difference: 
+  # it only gets called when a model is created for the first time. 
+  # This means not every time the object is persisted, just when it is new.
+
   private
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
+  end
+
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
   end
 
   def make_title_case
